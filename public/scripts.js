57df6bd294227f00aa82a601875f78ea9cfe9061
mussawer <mussawerrehan@gmail.com>
2020-04-28 04:13:37 -0700

    initial commit

---
+++ public/scripts.js
@@ -0,0 +1,70 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth * 3/4, window.innerHeight * 3/4);
+var node = document.getElementById("canvas");
+node.appendChild( renderer.domElement );
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setTexturePath('/assets/');
+mtlLoader.setPath('/assets/');
+mtlLoader.load('r2-d2.mtl', function (materials) {
+
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('/assets/');
+    objLoader.load('r2-d2.obj', function (object) {
+        
+        scene.add(object);
+        object.position.y -= 60;
+        
+
+    });
+
+});
+
+
+
+
+
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+
+
+camera.position.z = 200;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	// cube.rotation.x += 0.1;
+	// cube.rotation.y += 0.1;
+    controls.update();
+    console.log('animate');
+	renderer.render(scene, camera);
+};
+console.log('animate');
+animate();
